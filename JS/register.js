ea4963cf1d6c7e47d8b20db8cdfd0786461e3222
Erlan <erlankambarov94@gmail.com>
2022-04-22 00:36:57 +0600

    test4

--- JS/register.js
+++ JS/register.js
@@ -18,9 +18,7 @@ function getRegister() {
     })
     .then(res => res.json())
     .then(res => {
-        localStorage.setItem('accessToken', res.accessToken)
-        localStorage.setItem('refreshToken', res.refreshToken)
-        localStorage.setItem('userID', res.user.id)
+        console.log(res);
     })
 }
 
